2225cc85749fe653eeb47419f469b9472c00102f
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2019-06-28 11:32:15 +0300

    move the reminder to slack not to mail

--- index.js
+++ index.js
@@ -2,62 +2,60 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fetch = require('fetch')
-const nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS
-    }
-});
-var teamName 
-fetch.fetchUrl("https://slack.com/api/team.info?token="+ process.env.SLACK_KEY,(error,meta,body)=>{
-    let response = JSON.parse(body.toString());
-    teamName = response.name
-})
+
+
+let check_timer = 1000 * 60 * 60 * 24; // Check for unsolved pins oncea day
+
 app.listen(port,()=>{
+
     setInterval(()=>{
-        fetch.fetchUrl("https://slack.com/api/channels.list?token=" + process.env.SLACK_KEY,(error, meta, body)=>{
-
-            for(let c of JSON.parse(body.toString()).channels){
-                fetch.fetchUrl("https://slack.com/api/pins.list?token=" + process.env.SLACK_KEY + "&channel=" + c.id ,(error, meta, body)=>{
-
-                    for(let p of JSON.parse(body.toString()).items){
-                        if(new Date(Number(p.created * 1000) + 12096e5).getDay() = new Date()){
-                            fetch.fetchUrl("https://slack.com/api/channels.history?token=" + process.env.SLACK_KEY + "&channel=" + c.id ,(error, meta, body)=>{
-    
-                                let response = JSON.parse(body.toString())
-                                let rex = new RegExp("<@\.+>")
-                                let tags = response.messages.filter(t => t.text.match(rex))
-                                for(let i=0;i< response.messages.length;i++){
-                                    let match = response.messages[i].text.match(rex)
-                                    if(match){
-                                        let id = match[0].slice(2,match[0].length-1)
-    
-                                        fetch.fetchUrl("https://slack.com/api/users.info?token=" + process.env.SLACK_KEY + "&user=" + id ,(error, meta, body)=>{
-                                            let email = JSON.parse(body.toString()).user.profile.email
-                                            var mailOptions = {
-                                                from: process.env.EMAIL,
-                                                to: email,
-                                                subject: 'pending pin',
-                                                text: 'You have been tagged for a pin in ' + teamName + " in channel " + c.name
-                                            };
-                                            transporter.sendMail(mailOptions, function(error, info){
-                                                if (error) {
-                                                  console.log(error);
-                                                } else {
-                                                  console.log('Email sent: ' + info.response);
-                                                }
-                                            });
+        slackRequest({method: "channels.list" ,token: process.env.SLACK_KEY}, (body)=>{
+            let channels = body.channels
+            for(let c of channels){
+                slackRequest({method: "pins.list" ,token: process.env.SLACK_KEY, channel: c.id}, (body)=>{
+                    let items = body.items
+                    for(let p of items){
+                        if(new Date(Number(p.created * 1000) + 12096e5).getDay() >= new Date()){ // check if the pin creation date was two weeks or more before today 
+                            let rex = new RegExp("<@\.+>")
+                            let match = p.message.text.match(rex)
+                            if(match){
+                                let id = match[0].slice(2,match[0].length-1)
+                                slackRequest({method: "im.open" ,token: process.env.SLACK_BOT_KEY, user: id}, (body)=>{
+                                    let dm_id = body.channel.id
+                                    if(dm_id){
+                                        slackRequest({method:"chat.postMessage",token: process.env.SLACK_BOT_KEY,channel: dm_id,text: "You have an old pinned still not resolved in channel "+ c.name + "\n " + p.message.permalink}, (body)=>{
+                                            console.log(body)
                                         })
                                     }
-                                }
-                            })
+                                })
+                            }
                         }
                     }
                 })
             }
-        });
+        })
 
-    },2000)
+    },check_timer)
 })
+function parse(responseBody){
+    if(!responseBody){ throw Error('no body')}
+    return JSON.parse(responseBody.toString())
+}
+function slackRequest(params, callback){
+    let host = "https://slack.com/api/" + params["method"] + "?"
+    delete params["method"]
+    let uris = []
+    for(let p in params){
+        uris.push(p + "=" + params[p])
+    }
+    host += uris.join("&")
+    fetch.fetchUrl(host, (error, meta, body)=>{
+        if(error){
+            throw Error(error)
+        }
+        if(!body){
+            throw Error('no body for request' + host)
+        }
+        callback(parse(body))
+    })
+}
