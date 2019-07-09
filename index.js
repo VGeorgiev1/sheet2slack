817629119a6a0b4a2d9d2822c17aa0200ad416e0
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2019-07-09 18:53:52 +0300

    fixed

--- index.js
+++ index.js
@@ -10,28 +10,24 @@ const checkTimer =  1000 * 60 * 60 * 24; // Check for unsolved pins once per day
 
 app.listen(port, ()=>{
     setInterval(async()=>{
-        let response = await slackRequest(
+        slackRequest(
         {
             method: 'channels.list',
             token: process.env.SLACK_KEY
-        })
+        }).then((body)=>{
+            let channels = body.channels
 
-        let channels = response.channels
+            for (const c of channels) {
+                slackRequest(
+                {
+                    method: 'pins.list',
+                    token: process.env.SLACK_KEY,
+                    channel: c.id
 
-        for (const c of channels) {
-            slackRequest(
-            {
-                method: 'pins.list',
-                token: process.env.SLACK_KEY,
-                channel: c.id
-
-            }).then(async (body)=>{
-                const items = body.items;
-                for (const pin of items) {
-                    if (new Date(Number(pin.created * 1000) + 12096e5).getDay() >= new Date()) { // check if the pinin creation date was two weeks or more before today
-                        const rex = new RegExp('<@\.+>');
-                        const match = pin.message.text.match(rex);
-                        if (match) {
+                }).then(async (body)=>{
+                    const items = body.items;
+                    for (const pin of items) {
+                        if (new Date(Number(pin.created * 1000) + 12096e5).getDay() >= new Date()) { // check if the pinin creation date was two weeks or more before today    
                             slackRequest(
                             {
                                 method: 'conversations.replies',
@@ -40,31 +36,34 @@ app.listen(port, ()=>{
                                 ts: pin.message.ts
 
                             }).then((body)=>{
+                                const rex = new RegExp('<@\.+>');
                                 let sendTo = null;
                                 for(let i = body.messages.length - 1;i >= 0;i--){
-                                    let  match = body.messages[i].text.match(rex)
+                                    let match = body.messages[i].text.match(rex)
                                     if(match){
                                         sendTo = match[0]
                                         break;
                                     }
                                 }
-                                console.log(pin)
-                                slackRequest(
-                                {   
-                                    method: 'chat.postMessage',
-                                    token: process.env.SLACK_BOT_KEY,
-                                    channel: pin.channel,
-                                    thread_ts: pin.message.thread_ts || pin.message.ts,
-                                    text: sendTo + ' You have an unresolved pin'
-                                }).then((body)=>{
-                                    console.log(body)
-                                })
+                                if(sendTo.length != 0){
+                                    slackRequest(
+                                    {   
+                                        method: 'chat.postMessage',
+                                        token: process.env.SLACK_BOT_KEY,
+                                        channel: pin.channel,
+                                        thread_ts: pin.message.thread_ts || pin.message.ts,
+                                        text: sendTo + ' You have an unresolved pin'
+                                    }).then((body)=>{
+                                        console.log(body)
+                                    })
+                                }
                             })
+                            
                         }
                     }
-                }
-            })
-        }
+                })
+            }
+        })
         
     },checkTimer)
 });
